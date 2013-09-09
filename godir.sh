0b69976ccd16dd67f4a723823873b0284b50ee9c
arzh <jacobaarthur@gmail.com>
2013-09-09 01:01:38 -0500

    godir.sh - helper script to create a new workspace
    todo: automaticly add new workspace to GOPATH

---
+++ godir.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# Helper script for making a go workspace
+
+function usage
+{
+	echo ""
+	echo "godir: used for creating Go workspaces"
+	echo "--------------------------------------"
+	echo "usage:"
+	echo "  godir <workspace name> [-git <remote>]"
+	echo ""
+	echo "<workspace name>: name of root folder name for the workspace [required]"
+	echo ""
+	echo "--git | -g : initilize a git repository"
+	echo "    remote: git remote to set 'origin' to"
+	echo ""
+}
+
+workspace=
+initGit=0
+gitRemote=
+
+
+if [ "$1" = "" -o "$1" = "-h" -o "$1" = "--help" ]; then
+	usage
+	exit 1
+else
+	workspace=$1
+fi
+
+while [ "$2" != "" ]; do
+	case $2 in
+		-g | --git ) 	initGit=1
+				shift
+				gitRemote=$2	
+	esac
+
+	shift
+done
+
+if [ -e $workspace ]; then
+	echo "error: workspace path already exist"
+	exit 1
+fi	
+
+mkdir $workspace/bin -p
+mkdir $workspace/pkg -p
+mkdir $workspace/src -p
+
+cd $workspace
+
+if [ "$initGit" == 1 ]; then
+	echo "git init"
+	git init
+	echo "init finished"
+	echo "remote name:"
+	echo $gitRemote
+
+	if [ "$gitRemote" != "" ]; then
+		git remote add origin https://$gitRemote/$workspace.git
+	fi
+fi	
